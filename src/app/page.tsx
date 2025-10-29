85426d384064289a22ba64c2575fa17cacdf3e53
Alberto Acosta <albertoacosta1007@gmail.com>
2025-10-29 09:52:15 -0400

    Creacion del repositorio

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+export default function Home() {
+  return (
+    <div>
+      <p>Version 1</p>
+      <p>Los botones ya tienen que tener funcionalidades, es decir, crear todas las tablas de la base de datos(mira la planificacion y entiendela a profundidad)</p>
+      <p>Lo primero que debe aparecer en el sistema es un modulo de usuario para que se registre</p>
+      <p>Ademas de crear las tablas, crea el codigo que llame las tablas, que funcione el html</p>
+      <p>Pon el css o tailwind bien bonito</p>
+      <p>-</p>
+      <p>NAW Rentcar y Asociados. S.R.L</p>
+      <button>Registrarse</button>
+      <button>Alquilar vehiculo</button>
+      <button>Poner vehiculo en alquiler</button>
+    </div>
+  );
+}
+
